47f285f8266a5f0b8ebb97a1378de20f21ad9551
azhahd <116668504+azhahd@users.noreply.github.com>
2023-03-09 12:13:29 -0500

    Update Test.java
    I added a print statement that says bye

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         //this is a test
         System.out.println("Hello world.");
+        System.out.println("bye");
     }
     
 }
