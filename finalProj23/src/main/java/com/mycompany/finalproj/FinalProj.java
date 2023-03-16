1cea8f7b0ab846b612a7f426505c55398249b5eb
aariawann <aariawan@uoguelph.ca>
2023-03-16 16:20:54 -0400

    Added Laserfiche Repo API
    I added a project called 'finalProj23' it has a class called 'Sample' with all the Repo stuff in there.

---
+++ finalProj23/src/main/java/com/mycompany/finalproj/FinalProj.java
@@ -0,0 +1,25 @@
+/***************************************
+ * Project: 
+ * Programmer: Aris Ariawan
+ * Date: Month #, 2023
+ * Program: ----.java
+ ***************************************/
+package com.mycompany.finalproj;
+
+import com.laserfiche.repository.api.clients.impl.model.Entry;
+
+/**
+ *
+ * @author arisariawan
+ */
+public class FinalProj {
+
+    public static void main(String[] args) {
+        
+        
+        Entry lfEntry = new Entry();
+        
+        
+        
+    }
+}
