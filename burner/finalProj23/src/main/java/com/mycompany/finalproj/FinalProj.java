a08a8094517d81ca42f6bf3fe86f27a523a04059
aariawann <aariawan@uoguelph.ca>
2023-04-04 12:12:07 -0400

    With everything together

---
+++ burner/finalProj23/src/main/java/com/mycompany/finalproj/FinalProj.java
@@ -0,0 +1,30 @@
+/***************************************
+ * Project: 
+ * Programmer: Aris Ariawan
+ * Date: Month #, 2023
+ * Program: ----.java
+ ***************************************/
+package com.mycompany.finalproj;
+
+import com.laserfiche.repository.api.clients.impl.model.Entry;
+import com.mycompany.sampleproject.Sample;
+
+/**
+ *
+ * @author arisariawan
+ */
+public class FinalProj {
+
+    public static void main(String[] args) {
+        
+        
+       Sample file1 = new Sample(16);
+       Sample file2 = new Sample(17);
+       
+      file1.createFile();
+      file2.createFile();
+        
+        
+        
+    }
+}
